43d2e738278c13338374087418e7b99b200f30be
Omkar Kulkarni <omkar@Omkars-MacBook-Air.local>
2025-04-19 12:14:13 -0700

    test

--- app.py
+++ app.py
@@ -6,16 +6,31 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 import nltk
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from keybert import KeyBERT
+import json
+import google.generativeai as genai
+import re
+import base64
+import tempfile
+
 
 #from services.pdf_parser import parse_pdf
 #services.simplifier import simplify_text
 #services.flashcard_generator import generate_flashcards
 
 
-
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
+
+GEMINI_API_KEY = "AIzaSyDL0deo5LkDstEUevrH8SK5cwWZsgobpZg"
+
+if not GEMINI_API_KEY:
+    print("Warning: GEMINI_API_KEY not found in environment variables")
+
+if GEMINI_API_KEY:
+    genai.configure(api_key=GEMINI_API_KEY)
+    gemini_model = genai.GenerativeModel('gemini-pro')
+
 
 tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
 model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
@@ -35,6 +50,7 @@ def highlight_text(text):
 
 def generate_quiz():
     pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
