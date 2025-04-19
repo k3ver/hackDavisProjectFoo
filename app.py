cdc8962e4448a16827a9cb7a756b5277becdb60b
Omkar Kulkarni <omkar@campus-046-224.ucdavis.edu>
2025-04-18 23:02:50 -0700

    possible change

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+#from services.pdf_parser import parse_pdf
+#services.simplifier import simplify_text
+#services.flashcard_generator import generate_flashcards
+
+# Initialize Flask app
+app = Flask(__name__)
+CORS(app)
