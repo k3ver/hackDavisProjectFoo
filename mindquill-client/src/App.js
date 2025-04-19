175fd0e787e76dbe6d08aca2b6eeb3b759101eb8
Beckett Hayes <becketthayes@gmail.com>
2025-04-19 16:49:38 -0700

    new folder

---
+++ mindquill-client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+import { Desktop } from "./Desktop"; // make sure the path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <Desktop />
+    </div>
+  );
+}
+
+export default App;
