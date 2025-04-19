21ee368479f18e57afe68d2569dea7030e2433cb
Beckett Hayes <becketthayes@gmail.com>
2025-04-18 23:45:52 -0700

    changed app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
 #from services.pdf_parser import parse_pdf
 #services.simplifier import simplify_text
 #services.flashcard_generator import generate_flashcards
