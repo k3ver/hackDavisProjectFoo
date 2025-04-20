6d276c9a2b1e8be0d8519dedd7e3e73222efd3bb
k3verOSX <183549436+k3ver@users.noreply.github.com>
2025-04-20 09:02:39 -0700

    API stuff again

--- backend/app.py
+++ backend/app.py
@@ -195,6 +195,6 @@ def verify_token():
         return jsonify({"status": "error", "message": "Invalid token"}), 401
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=False) 
 
